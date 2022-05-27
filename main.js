ff51f2c6bbb1ba7c2727abcd6db58b55127c91ca
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-27 18:35:48 +0200

    Now circles are only drawn when left mouse button is pressed. Initially this was on the canvas element but it feels way more intuitive to have it on the body element

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+var body = document.getElementsByTagName("body");
 var canvas = document.getElementById("drawingArea");
 var context = canvas.getContext("2d");
+var leftMousebtnPressed = false;
+body[0].addEventListener("mouseup", function () {
+    leftMousebtnPressed = false;
+});
+body[0].addEventListener("mousedown", function () {
+    leftMousebtnPressed = true;
+});
 canvas.addEventListener("mousemove", function (mouseEvent) {
-    var x = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
-    var y = mouseEvent.offsetY;
-    drawCircle(x, y);
+    if (leftMousebtnPressed) {
+        var x = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
+        var y = mouseEvent.offsetY;
+        drawCircle(x, y);
+    }
 });
 function drawCircle(x, y) {
     context.beginPath();
     context.arc(x, y, 5, 0, Math.PI * 2);
     context.fill();
 }
-// function draw(){
-//     context.clearRect(0,0,canvas.width,canvas.height);
-//     requestAnimationFrame(draw);
-// }
--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
+const body = document.getElementsByTagName("body") as HTMLCollectionOf<HTMLBodyElement>;
 const canvas = document.getElementById("drawingArea") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
+let leftMousebtnPressed:boolean = false;
+
+
+body[0].addEventListener("mouseup", () => {
+    leftMousebtnPressed = false;
+});
+
+body[0].addEventListener("mousedown", () => {
+    leftMousebtnPressed = true;
+});
+
 
 canvas.addEventListener("mousemove", (mouseEvent) => {
-    const x:number = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
-    const y:number = mouseEvent.offsetY;
-    drawCircle(x,y);
+    if(leftMousebtnPressed){
+        const x:number = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
+        const y:number = mouseEvent.offsetY;
+        drawCircle(x,y);
+    }
+    
 });
 
 function drawCircle(x, y){
