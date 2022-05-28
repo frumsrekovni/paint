8df221600d66d28a2f3864222a4ecf0785c23e18
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-28 17:28:41 +0200

    Drawn circles now connect to create a continous line

--- main.js
+++ main.js
@@ -2,21 +2,37 @@ var body = document.getElementsByTagName("body");
 var canvas = document.getElementById("drawingArea");
 var context = canvas.getContext("2d");
 var leftMousebtnPressed = false;
+var posX = 0;
+var posY = 0;
+var brushSize = 10;
 body[0].addEventListener("mouseup", function () {
     leftMousebtnPressed = false;
 });
-body[0].addEventListener("mousedown", function () {
+body[0].addEventListener("mousedown", function (mouseEvent) {
+    drawCircle(mouseEvent.offsetX, mouseEvent.offsetY);
     leftMousebtnPressed = true;
+    posX = mouseEvent.offsetX;
+    posY = mouseEvent.offsetY;
 });
 canvas.addEventListener("mousemove", function (mouseEvent) {
     if (leftMousebtnPressed) {
-        var x = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
-        var y = mouseEvent.offsetY;
-        drawCircle(x, y);
+        var xx = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
+        var yy = mouseEvent.offsetY;
+        drawCircle(xx, yy);
+        connectCircles(posX, posY, xx, yy);
+        posX = xx;
+        posY = yy;
     }
 });
 function drawCircle(x, y) {
     context.beginPath();
-    context.arc(x, y, 5, 0, Math.PI * 2);
+    context.arc(x, y, brushSize, 0, Math.PI * 2);
     context.fill();
+}
+function connectCircles(x, y, xx, yy) {
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(xx, yy);
+    context.lineWidth = brushSize * 2;
+    context.stroke();
 }
--- main.ts
+++ main.ts
@@ -2,28 +2,44 @@ const body = document.getElementsByTagName("body") as HTMLCollectionOf<HTMLBodyE
 const canvas = document.getElementById("drawingArea") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 let leftMousebtnPressed:boolean = false;
-
+let posX:number = 0;
+let posY:number = 0;
+let brushSize:number = 10;
 
 body[0].addEventListener("mouseup", () => {
     leftMousebtnPressed = false;
 });
 
-body[0].addEventListener("mousedown", () => {
+body[0].addEventListener("mousedown", (mouseEvent) => {
+    drawCircle(mouseEvent.offsetX,mouseEvent.offsetY);
     leftMousebtnPressed = true;
+    posX = mouseEvent.offsetX;
+    posY = mouseEvent.offsetY;
 });
 
 
 canvas.addEventListener("mousemove", (mouseEvent) => {
     if(leftMousebtnPressed){
-        const x:number = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
-        const y:number = mouseEvent.offsetY;
-        drawCircle(x,y);
+        const xx:number = mouseEvent.offsetX; // If you use just X here the point of the mouse is where the left corner of the circle is drawn instead of the center
+        const yy:number = mouseEvent.offsetY;
+        drawCircle(xx,yy);
+        connectCircles(posX,posY,xx,yy);
+        posX = xx;
+        posY = yy;
     }
     
 });
 
-function drawCircle(x, y){
+function drawCircle(x:number, y:number){
     context.beginPath();
-    context.arc(x,y,5,0,Math.PI*2);
+    context.arc(x,y,brushSize,0,Math.PI*2);
     context.fill();
+}
+
+function connectCircles(x:number,y:number,xx:number,yy:number){
+    context.beginPath();
+    context.moveTo(x,y);
+    context.lineTo(xx,yy);
+    context.lineWidth = brushSize*2;
+    context.stroke();
 }
